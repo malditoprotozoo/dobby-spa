562486950f06186d342e550bcb8321517e563399
Tori Rodríguez <virodriguez.fe@gmail.com>
2018-02-12 10:42:47 -0300

    nav ready

---
+++ assets/js/app.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  objFunctions.limitWidthImg('#slider img', '100%');
+  objFunctions.limitWidthImg('.thumb', '180px');
+  objFunctions.changeHeightImg('.thumb', '180px', true);
+});
