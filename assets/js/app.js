d0171a4b0a33ed3728e200254a865de6bfeb23f1
Tori Rodríguez <virodriguez.fe@gmail.com>
2018-02-12 11:37:16 -0300

    adding more elements and design

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
   objFunctions.limitWidthImg('#slider img', '100%');
-  objFunctions.limitWidthImg('.thumb', '180px');
-  objFunctions.changeHeightImg('.thumb', '180px', true);
+  objFunctions.limitWidthImg('.thumb', '200px');
+  objFunctions.changeHeightImg('.thumb', '200px', true);
 });
